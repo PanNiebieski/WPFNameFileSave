04c12eaa8319d7bb98511a596b338e0040569557
Cezary Walenciuk <cezarywalenciuk@gmail.com>
2022-09-06 08:27:31 +0200

    Add project files.

---
+++ WPFNameFileSave/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace WPFNameFileSave
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfim_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter writer = new StreamWriter("Hi_My_Name_Is.txt"))
+            {
+                writer.Write("Zapisuje tekst");
+                writer.WriteLine("Zapisuje tekst i tworze znak nowej lini");
+                writer.WriteLine(tb_Name.Text);
+                writer.WriteLine(tb_SurName.Text);
+            }
+        }
+    }
+}
